840817f7c0278cc88639ff530f1934b51408ce1c
manas2007 <manas.st123@gmail.com>
2025-10-10 04:56:36 +0530

    Website Logo :: Title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <title>Give Your Best | Om Prakash Dhankar</title>
+        <link rel="icon" href="/assets/images/logo.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
